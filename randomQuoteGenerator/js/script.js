f0d3475cd1f22654aae9dc2bbaf60eddecc2a1ae
Jason Sikes <sikeswebdevelopment@gmail.com>
2018-07-28 07:49:22 -0500

    Added in basic js codes without quotes

--- randomQuoteGenerator/js/script.js
+++ randomQuoteGenerator/js/script.js
@@ -3,20 +3,30 @@
 // Create the array of quote objects and name it quotes
 let quotes = [
   {
-
-  }
+    quote: "",
+    source: "",
+    citation: "",
+    year: ""
+  },
 ]
 
 
 // Create the getRandomQuote function and name it getRandomQuote
 function getRandomQuote() {
-  
+  for (let i=0; i<quotes.length; i+=1){
+    let randomNum = Math.floor(Math.random() * quotes.length)+1;
+    let index = quotes.indexOf(i);
+    if (index === randomNum) {
+    printQuote(i);
+    }
+  }
 }
 
 
 // Create the printQuote funtion and name it printQuote
 function printQuote(quote) {
-
+  let quoteP = document.getElementsByClassName("quote");
+  quoteP.innerHTML = quote
 }
 
 
